62827c7aadd3d013a534f6dc8dc5bab8239b2d59
sam4rano <oyerindesamuelabiodun@gmail.com>
2022-12-19 08:42:29 +0100

    aa

--- pages/Login.js
+++ pages/Login.js
@@ -1,47 +1,16 @@
 import Link from "next/link";
-import React, { useEffect } from "react";
+import React from "react";
 import Layout from "../components/Layout";
-import { signIn, useSession } from "next-auth/react";
 import { useForm } from "react-hook-form";
-import { getError } from "../utils/error";
-import { toast } from "react-toastify";
-import { useRouter } from "next/router";
-
- 
-
 
 const Login = () => {
-
-  const {data: session} = useSession();
-
-  const router = useRouter();
-  const { redirect } = router.query;
-
-
-  useEffect(() => {
-    if (session?.user) {
-      router.push(redirect || '/');
-    }
-  }, [router, session, redirect]);
-
   const {
     handleSubmit,
     register,
     formState: { errors },
   } = useForm();
   const submitHandler = async({ email, password }) => {
-    try {
-      const result = await signIn('credentials', {
-        redirect: false,
-        email,
-        password,
-      });
-      if (result.error) {
-        toast.error(result.error);
-      }
-    } catch(err){
-      toast.error(getError(err))
-    }
+    console.log(email, password);
   };
   return (
     <Layout title="Login">
@@ -66,12 +35,10 @@ const Login = () => {
             id="email"
             autoFocus
           ></input>
-          {errors.email && (
-            <div className="text-red-500">{errors.email.message}</div>
-          )}
+          {errors.email && <div>{errors.email.message}</div>}
         </div>
         <div className="mb-4">
-          <label htmlFor="password">Password</label>
+          <label>Password</label>
           <input
             type="password"
             className="w-full"
@@ -82,9 +49,6 @@ const Login = () => {
               minLength: { value: 6, message: "password is more than 5 chars" },
             })}
           ></input>
-          {errors.password && (
-            <div className="text-red-500 ">{errors.password.message}</div>
-          )}
         </div>
         <div className="mb-4">
           <button className="primary-button">Login</button>
