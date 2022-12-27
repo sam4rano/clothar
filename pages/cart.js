db907614243bb582219488de85ce16f3035c342a
sam4rano <oyerindesamuelabiodun@gmail.com>
2022-12-27 23:56:06 +0100

    product screen

--- pages/cart.js
+++ pages/cart.js
@@ -6,6 +6,8 @@ import Layout from "../components/Layout";
 import { Store } from "../utils/Store";
 import { useRouter } from "next/router";
 import dynamic from "next/dynamic";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Cart = () => {
   const { state, dispatch } = useContext(Store);
@@ -18,10 +20,16 @@ const Cart = () => {
     dispatch({type: 'CART_REMOVE_ITEM', payload:item});
   };
 
-  const updateCartHandler =  (item, qty) => {
+  const updateCartHandler = async (item, qty) => {
     const quantity = Number(qty);
+    const { data } = await axios.get(`/api/products/${item._id}`);
+    if (data.countInStock < quantity) {
+      return toast.error("Sorry. Product is out of stock");
+    }
     dispatch({ type: "CART_ADD_ITEM", payload: { ...item, quantity } });
+    toast.success("Product updated in the cart");
   };
+  
   return (
     <Layout title="Shopping Cart">
       <h1 className="mb-4 text-xl">Shopping Cart</h1>
