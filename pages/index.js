6ac96ce296139de70be1b8591daac70e1c45adcd
sam4rano <oyerindesamuelabiodun@gmail.com>
2023-02-13 11:25:51 +0100

    eslint and pay, deliver et index.js

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,7 @@ import Link from "next/link";
 export default function Home({ products, featuredProducts }) {
   const { state, dispatch } = useContext(Store);
   const { cart } = state;
+  
 
   const addToCartHandler = async (product) => {
     const existItem = cart.cartItems.find((x) => x.slug === product.slug);
